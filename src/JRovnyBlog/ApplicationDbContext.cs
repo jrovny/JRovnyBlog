0863a5269286094906562db5c2fac970af516688
jrovny <jrovny@JROVNYDELL7540>
2021-02-24 21:48:16 -0600

    Inject connection service and connection string

--- src/JRovnyBlog/ApplicationDbContext.cs
+++ src/JRovnyBlog/ApplicationDbContext.cs
@@ -4,6 +4,16 @@ namespace JRovnyBlog
 {
     public class ApplicationDbContext : DbContext
     {
+        private readonly IConnectionService _connectionService;
 
+        public ApplicationDbContext(IConnectionService connectionService)
+        {
+            _connectionService = connectionService;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseNpgsql(_connectionService.GetDefaultConnectionString());
+        }
     }
 }
