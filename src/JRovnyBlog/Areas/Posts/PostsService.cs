285e12ec56e53e5238ca1e3b34e807d9146a595b
jrovny <jrovny@JROVNYDELL7540>
2021-02-26 23:47:54 -0600

    Add function to get all blog posts

--- src/JRovnyBlog/Areas/Posts/PostsService.cs
+++ src/JRovnyBlog/Areas/Posts/PostsService.cs
@@ -1,6 +1,12 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
 namespace JRovnyBlog.Areas.Posts
 {
-    public class PostsService
+
+    public class PostsService : IPostsService
     {
         private readonly ApplicationDbContext _context;
 
@@ -9,6 +15,22 @@ namespace JRovnyBlog.Areas.Posts
             _context = context;
         }
 
-
+        public async Task<IEnumerable<PostSummary>> GetAllBlogPostsAsync()
+        {
+            return await _context.Posts
+                .AsNoTracking()
+                .Where(p => p.Published == true)
+                .Select(p => new PostSummary
+                {
+                    PostId = p.PostId,
+                    Title = p.Title,
+                    Slug = p.Slug,
+                    Image = p.Image,
+                    CommentCount = p.CommentCount,
+                    UpvoteCount = p.UpvoteCount,
+                    CreatedDate = p.CreatedDate
+                })
+                .ToListAsync();
+        }
     }
 }
