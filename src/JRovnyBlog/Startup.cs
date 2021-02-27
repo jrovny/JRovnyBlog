548edd55036de06622fdd083bae9b4345e314fb9
jrovny <jrovny@JROVNYDELL7540>
2021-02-26 23:47:25 -0600

    Add PostsService to DI

--- src/JRovnyBlog/Startup.cs
+++ src/JRovnyBlog/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JRovnyBlog.Areas.Posts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +26,7 @@ namespace JRovnyBlog
         {
             services.AddSingleton<IConnectionService, ConnectionService>();
             services.AddDbContext<ApplicationDbContext>();
+            services.AddTransient<IPostsService, PostsService>();
             services.AddRazorPages();
         }
 
