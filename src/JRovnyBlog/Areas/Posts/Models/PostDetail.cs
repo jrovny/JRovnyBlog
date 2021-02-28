c921db9d99ee979913a63250aea5873305bfd8d4
jrovny <jrovny@JROVNYDELL7540>
2021-02-27 18:27:15 -0600

    Add PostDetail model for main post page

---
+++ src/JRovnyBlog/Areas/Posts/Models/PostDetail.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace JRovnyBlog.Areas.Posts.Models
+{
+    public class PostDetail
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Slug { get; set; }
+        public string Image { get; set; }
+        public int ViewCount { get; set; }
+        public int CommentCount { get; set; }
+        public int UpvoteCount { get; set; }
+        public int DownvoteCount { get; set; }
+        public bool Published { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
