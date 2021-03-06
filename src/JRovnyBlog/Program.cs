da8b36fce49cebd5d67dffb631777ad1eb952ca5
jrovny <jrovny@JROVNYDELL7540>
2021-03-06 01:21:52 -0600

    Configure Serilog

--- src/JRovnyBlog/Program.cs
+++ src/JRovnyBlog/Program.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
 
 namespace JRovnyBlog
 {
@@ -18,6 +15,19 @@ namespace JRovnyBlog
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, logger) =>
+                {
+                    var isDevelopment = context.HostingEnvironment.IsDevelopment();
+                    logger.MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning);
+                    logger.Enrich.FromLogContext();
+                    if (isDevelopment)
+                        logger.WriteTo.Console();
+                    logger.WriteTo.File(new JsonFormatter(),
+                        isDevelopment ? @".\log\log-.txt" : @"./log/log-.txt",
+                        rollingInterval: RollingInterval.Day,
+                        retainedFileCountLimit: null,
+                        rollOnFileSizeLimit: true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- src/JRovnyBlog/Startup.cs
+++ src/JRovnyBlog/Startup.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using JRovnyBlog.Areas.Posts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace JRovnyBlog
 {
@@ -48,9 +44,8 @@ namespace JRovnyBlog
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
+            app.UseSerilogRequestLogging();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
