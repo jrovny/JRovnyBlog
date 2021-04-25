a20e621366168f1f5e83ec2c46f33c74cdd52bae
jrovny <jrovny@JROVNYDELL7540>
2021-04-24 22:31:34 -0500

    Create comment view model

---
+++ src/JRovnyBlog/Models/Comment.cs
@@ -0,0 +1,9 @@
+namespace JRovnyBlog.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public int PostId { get; set; }
+        public string Content { get; set; }
+    }
+}
