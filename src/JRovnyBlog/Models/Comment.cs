0181f9571a2f82abcc022ebf25734fd9808428ae
jrovny <jrovny@JROVNYDELL7540>
2021-04-26 22:06:58 -0500

    Add comment indenting

--- src/JRovnyBlog/Models/Comment.cs
+++ src/JRovnyBlog/Models/Comment.cs
@@ -5,6 +5,7 @@ namespace JRovnyBlog.Models
     public class Comment
     {
         public int CommentId { get; set; }
+        public int? ParentCommentId { get; set; }
         public int PostId { get; set; }
         public string Content { get; set; }
         public string UserName { get; set; }
