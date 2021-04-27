92441d945d524f87dd0433e00f09feea013b3745
jrovny <jrovny@JROVNYDELL7540>
2021-04-26 21:30:48 -0500

    Add avatar icon to comments

--- src/JRovnyBlog/Models/Comment.cs
+++ src/JRovnyBlog/Models/Comment.cs
@@ -8,6 +8,7 @@ namespace JRovnyBlog.Models
         public int PostId { get; set; }
         public string Content { get; set; }
         public string UserName { get; set; }
+        public string EmailHash { get; set; }
         public DateTime CreatedDate { get; set; }
     }
 }
