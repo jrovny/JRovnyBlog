1c0cb9adc38fff3773c7c50985df273e840bf232
Jordan Rovny <rovnyjordan@gmail.com>
2021-10-01 21:22:59 -0500

    Modify to get google analytics id from appsettings

--- src/JRovnyBlog/TagHelpers/GoogleAnalyticsTagHelperComponent.cs
+++ src/JRovnyBlog/TagHelpers/GoogleAnalyticsTagHelperComponent.cs
@@ -1,26 +1,34 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Configuration;
 
 namespace JRovnyBlog.TagHelpers
 {
     public class GoogleAnalyticsTagHelperComponent : TagHelperComponent
     {
-        string _measurementId = "G-NFBCMHEYCK";
+        private readonly IConfiguration _configuration;
+
+        public GoogleAnalyticsTagHelperComponent(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
 
         public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             if (string.Equals(context.TagName, "head", StringComparison.OrdinalIgnoreCase))
             {
+                string measurementId = _configuration["GoogleAnalytics:MeasurementId"];
+
                 output.PostContent.AppendHtml(
-                    $@"<script async src=""https://www.googletagmanager.com/gtag/js?id={_measurementId}""></script>");
+                    $@"<script async src=""https://www.googletagmanager.com/gtag/js?id={measurementId}""></script>");
 
                 output.PostContent.AppendHtml(@"<script>
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());");
 
-                output.PostContent.AppendHtml($@"gtag('config', '{_measurementId}');</script>");
+                output.PostContent.AppendHtml($@"gtag('config', '{measurementId}');</script>");
             }
 
             return base.ProcessAsync(context, output);
